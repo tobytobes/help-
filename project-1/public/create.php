d5790bdc367609ab4463dfa351627ef286d3cda1
Toby Van der Zwart <69238298+tobytobes@users.noreply.github.com>
2020-08-13 22:56:59 +1000

    Update create.php

--- project-1/public/create.php
+++ project-1/public/create.php
@@ -4,7 +4,7 @@ frequently at it? i think you know the answer to that...-->
 <?php
 
 if (isset($_POST['submit'])) {
-    require "../config";
+    require "../config.php";
 		
 		try {
 		$connection = new PDO($dsn, $username, $password, $options);
