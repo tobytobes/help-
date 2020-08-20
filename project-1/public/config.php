fdfc6f7333165017c00d067500b184fa75cbc1a3
Toby Van der Zwart <69238298+tobytobes@users.noreply.github.com>
2020-08-20 11:41:17 +1000

    Update config.php

--- project-1/public/config.php
+++ project-1/public/config.php
@@ -3,7 +3,7 @@
 $host       = "localhost";
 $username   = "root";
 $password   = "root";
-$dbname     = "test"; 
+$dbname     = "toby_test"; 
 $dsn        = "mysql:host=$host;dbname=$dbname"; 
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
